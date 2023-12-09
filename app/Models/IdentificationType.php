3cda700f776ff135e25b1a2740f80af25b3ab801
Nigel Jeche <jechekudzie@gmail.com>
2023-12-09 10:01:55 +0200

    final version 1.0

---
+++ app/Models/IdentificationType.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class IdentificationType extends Model
+{
+    use HasFactory;
+
+    protected $guraded = [];
+
+    public function practitioners(){
+
+        return $this->hasMany(Practitioner::class);
+
+    }
+}
---
+++ database/migrations/2023_12_06_103332_create_practitioner_identifications_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('practitioner_identifications', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('practitioner_id');
+            $table->unsignedBigInteger('identificaction_type_id');
+            $table->string('identification')->nullable();
+            $table->string('identification_file')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('practitioner_identifications');
+    }
+};
